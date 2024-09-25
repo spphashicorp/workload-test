b86e05fb8da3ab3ea53b2bbf9595b576c019b730
Stephane Pericat <stephane.pericat@hashicorp.com>
2024-09-25 14:17:32 -0500

    actions

--- test.js
+++ test.js
@@ -1,5 +1,14 @@
-console.log(
-  'hello, world!',
-  process.env.ACTIONS_ID_TOKEN_REQUEST_URL,
-  process.env.ACTIONS_ID_TOKEN_REQUEST_TOKEN,
-)
+import {
+  getIDToken
+} from '@actions/core'
+
+// console.log(
+//   'hello, world!',
+//   process.env.ACTIONS_ID_TOKEN_REQUEST_URL,
+//   process.env.ACTIONS_ID_TOKEN_REQUEST_TOKEN,
+// )
+const main = async () => {
+  const token = await getIDToken()
+}
+
+main()
